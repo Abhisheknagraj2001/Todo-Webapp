0d0cfd2bb56804bd7316cdcdd63ea7d7616594c3
Abhisheknagraj2001 <abhisheknagraj2001@gmail.com>
2025-08-26 18:30:43 +0530

    LocalStroage is added

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -3,9 +3,14 @@ import "./Todo.css";
 import { TodoForm } from "./TodoForm";
 import { TodoList } from "./TodoList";
 import { TodoDate } from "./TodoDate";
+import { getLocalStorageTodoData, setLocalStorageTodoData } from "./TodoLocalStorage";
+
+
+
+
 
 export const Todo = () => {
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState(()=>getLocalStorageTodoData());
 
   const handleFormSubmit = (inputValue) => {
     const {id,content,checked}=inputValue;
@@ -19,6 +24,8 @@ export const Todo = () => {
 
     setTask((prevTask) => [...prevTask, {id:id,content:content,checked:checked}]);
   };
+
+  setLocalStorageTodoData(task);
 
   const handleDeleteTodo = (value) => {
     const updatedTask = task.filter((curTask) => curTask.content !== value);
